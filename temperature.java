15e27fd2bb1358271325925d6c66e3cf1235dc13
feiacui <cuifei270@163.com>
2017-08-02 18:06:11 +0800

    FIRST

---
+++ temperature.java
@@ -0,0 +1,15 @@
+
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int f;
+		double c;
+		Scanner in=new Scanner(System.in);
+		f=in.nextInt();
+		c=(f-32)*5/9;
+		System.out.println((int)c);
+	}
+
+}
